169121f75f0d75f2f851900039dbb1703b9950b9
Imani Dinkins <izdinkins@aggies.ncat.edu>
2026-05-24 17:01:05 -0400

    first push

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -1,16 +1,18 @@
 public class TemperatureConverter {
 
-    /* hint
+
 
     public static double convertTemperature(double temperature, String unit) {
         // TODO: students implement this
+
         return 0.0;
     }
 
-     */
 
     public static void main(String[] args) {
         // TODO: students implement this
+
+        //hello this is a test
     }
 
 }
